2104c37c917539b7fd0ac0a995d6346d61cd2f36
SamVeiga <veigafotos3@gmail.com>
2025-07-11 23:39:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ except:
 # === CONFIGURAVEIS ===
 DONO_ID = 1481389775
 ID_GRUPO = -1002363575666
-MULHERES = ["@user1", "@user2"]  # Substitua pelos @ reais das mulheres
-HOMENS = ["@zeca", "@joao", "@diego"]  # Substitua pelos @ reais dos homens
+MULHERES = ["@KarolinneDiass", "@FernandaCarvalho16"]  # Substitua pelos @ reais das mulheres
+HOMENS = ["@Rafaael80", "@Diegomota0"]  # Substitua pelos @ reais dos homens
 
 # === FRASES ===
 insultos_gerais = [
